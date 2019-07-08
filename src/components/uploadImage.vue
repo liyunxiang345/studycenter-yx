27c295cedfcfa76e4d79481457f6e0e26f9c5456
liyunxiang <liyunxiang@vchangyi.com>
2019-07-08 17:46:56 +0800

    上传图片组件初代目

--- src/components/uploadImage.vue
+++ src/components/uploadImage.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="upload-image">
+  <div
+    class="upload-image"
+    @mouseover.prevent="is_hover = true"
+    @mouseleave.prevent="is_hover = false"
+  >
+    <!-- 表单层 -->
     <input class="real-form" type="file" ref="input" :value="file" @change="getFile($event)" />
-    <div class="upload-ui-wrap">
+    <!-- 上传层 -->
+    <div class="upload-ui-wrap" v-if="!is_upload">
       <div class="upload-before">
         <i class="el-icon-plus upload-icon"></i>
       </div>
+    </div>
+    <!-- 展示层 -->
+    <div class="upload-done" v-if="is_upload" :style="{backgroundImage:`url(${temp_url})`}">
+      <div class="upload-mask" v-show="is_upload && is_hover"></div>
     </div>
   </div>
 </template>
@@ -30,10 +40,16 @@ export default {
   data() {
     return {
       file: null, //文件
-      file_message: null //文件的信息
+      file_message: null, //文件的信息
+      temp_url: "", //测试开发阶段临时图片
+      is_upload: false,
+      is_hover: false //鼠标是否移动到其中
     };
   },
   methods: {
+    test() {
+      console.log(111);
+    },
     getFile(e) {
       if (e.target.files.length > 0) {
         this.file_message = e.target.files[0];
@@ -54,6 +70,7 @@ export default {
       return new Promise((resolve, reject) => {
         if (_this.beforeUpload(file)) {
           let url = window.URL.createObjectURL(file);
+          this.temp_url = url;
           let img = new Image();
           img.src = url;
           img.onload = () => {
@@ -63,6 +80,7 @@ export default {
             if (img.height > _this.sizelimit.height) {
               reject(`图片高度不能超过${_this.sizelimit.height}px`);
             }
+            this.is_upload = true;
             resolve();
           };
         } else {
@@ -86,7 +104,11 @@ export default {
   width: 148px;
   height: 148px;
   border: 1px dashed #c0ccda;
+  border-radius: 6px;
   position: relative;
+  &:hover {
+    border-color: #409eff;
+  }
   .upload-ui-wrap {
     width: 148px;
     height: 148px;
@@ -105,6 +127,23 @@ export default {
         font-size: 28px;
         color: #8c939d;
       }
+    }
+  }
+  .upload-done {
+    width: 148px;
+    height: 148px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-size: cover;
+    .upload-mask {
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+      position: absolute;
+      border-radius: 6px;
+      top: 0;
+      left: 0;
     }
   }
   .real-form {
